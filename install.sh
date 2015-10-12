6607fd3c17a4b1c24662576804801ca2e4919295
Gonzalo Matheu <gonzalommj@gmail.com>
2015-10-12 20:17:48 -0300

    Preventing installation if shellcheck is already installed

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -eo pipefail
 
-main() {
+version() {
+  echo "Version: $(shellcheck --version)"
+}
+
+install() {
   local filename="shellcheck_0.3.7-1_amd64.deb"
   wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
   sudo dpkg -i "$filename"
+}
+
+main() {
+  if (which shellcheck >& /dev/null)
+  then
+    echo "Shellcheck already installed"
+  else
+    install
+  fi
+  version
 }
 
 main
